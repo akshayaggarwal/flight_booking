4ada4434c15db5e0b89660377b0d95dd722d1316
Akshay <akadelhi1994@gmail.com>
2017-08-07 18:33:51 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <div class="container">
+            <div class="login-box">
+                <form method="post" action="submit_request.php">
+                    <h4>Login</h4>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input name="login_email" class="mdl-textfield__input" type="text" id="login_email">
+                        <label class="mdl-textfield__label" for="login_email">Request</label>
+                    </div><br>
+                    <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" name="submit" type="submit" value="Submit"/>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
